a4a1b837481819e99aa85ea0e92402b9e74d592e
Enrique Antunano <enriqueantunano@pop-os.localdomain>
2025-02-11 16:06:00 -0800

    eep522a c library templates for laser controller

---
+++ eep522a_embedded_and_real-time_systems/submissions/workspace/lib/characterization_lib.h
@@ -0,0 +1,46 @@
+
+/*
+ * MODULE: 
+ *
+ * characterization library header
+ *
+ * DESCRIPTION: 
+ * This library contains functions that are useful for Raspberry Pi 4 device characterization.
+ * 
+ * ACKNOWLEDGEMENTS:
+ * This library borrows from Andrew N. Sloss' prototype.c module developed for EP522A Embedded and Real-Time Systems course
+ * 
+ */
+
+#ifndef _CHARACTERIZATION_H_
+#define _CHARACTERIZATION_H_
+
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORTS                                  */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                 Data Types                                 */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                   Globals                                  */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                  CONSTANTS                                 */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------- Read / Write Speed Test Constants ------------------- */
+const int KiB           = 1024;
+const int MiB           = (1024*KiB);
+const int TEN_MiB       = (10*MiB);
+const int HUNDRED_MiB   = (100*MiB)
+
+/* -------------------------------------------------------------------------- */
+/*                                  Routines                                  */
+/* -------------------------------------------------------------------------- */
+
+
+
+#endif
---
+++ eep522a_embedded_and_real-time_systems/submissions/workspace/src/characterization_lib.c
@@ -0,0 +1,41 @@
+/*
+ * MODULE: 
+ *
+ * characterization library
+ *
+ * DESCRIPTION: 
+ * This library contains functions that are useful for Raspberry Pi 4 device characterization.
+ * 
+ * ACKNOWLEDGEMENTS:
+ * This library borrows from Andrew N. Sloss' prototype.c module developed for EP522A Embedded and Real-Time Systems course
+ * 
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORTS                                  */
+/* -------------------------------------------------------------------------- */
+
+/* --------------------------- standard libraries --------------------------- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+
+/* ----------------------------- local libraries ---------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                 Data Types                                 */
+/* -------------------------------------------------------------------------- */
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   Globals                                  */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                  Routines                                  */
+/* -------------------------------------------------------------------------- */
