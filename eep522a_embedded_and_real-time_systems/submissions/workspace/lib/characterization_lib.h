8c88078b4b6d29e08c1e2421802a4776337e12ad
Enrique Antunano <enriqueantunano@pop-os.localdomain>
2025-02-12 02:00:19 -0800

    eep522 file system test created. initial 128*MiB Buffer writes and GiB dummy files caused Raspbian to crash. Power cycle resolved issue and changed to 32*MiB buffer and 10MiB dummy files prevent raspberry pi from crashing

--- eep522a_embedded_and_real-time_systems/submissions/workspace/lib/characterization_lib.h
+++ eep522a_embedded_and_real-time_systems/submissions/workspace/lib/characterization_lib.h
@@ -41,6 +41,7 @@ const int KiB           = 1024;
 const int MiB           = (1024*KiB);
 const int TEN_MiB       = (10*MiB);
 const int HUNDRED_MiB   = (100*MiB);
+const int GiB           = (1024*MiB);
 
 /* -------------------------------------------------------------------------- */
 /*                                  Routines                                  */
@@ -66,8 +67,28 @@ double ram_write_time (
     size_t element_size
 );
 
+// Function to copy a file and measure time for File System
+double file_sys_time(
+    const char *src_filename, 
+    const char *dest_filename, 
+    size_t size, 
+    size_t element_size
+);
+
+// Helper function to create dummy files
+int create_dummy_file(
+    const char *filename,
+    size_t total_size,
+    size_t element_size
+);
+
 // Function runs all RAM write tests
 int ram_write_test (
+    char *base_path
+);
+
+// Function runs all File System write tests
+int file_sys_test (
     char *base_path
 );
 
